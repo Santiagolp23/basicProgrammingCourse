14514741801c4ef9f06810bd7e5e5898347e09ac
Santiagolp23 <santiagolopezpardo@gmail.com>
2022-03-01 21:34:23 -0500

    Drawing a border

--- canvas/drawing.js
+++ canvas/drawing.js
@@ -1,8 +1,24 @@
 var d = document.getElementById("drawing");
 var canvas = d.getContext("2d");
-canvas.beginPath();
-canvas.strokeStyle = "red";
-canvas.moveTo(100, 100);
-canvas.lineTo(200, 200);
-canvas.stroke();
-canvas.closePath();
+var lines = 30;
+var counter = 0;
+var yi, xf;
+
+function drawLine(color, xInitial, yInitial, xFinal, yFinal) {
+
+    canvas.beginPath();
+    canvas.strokeStyle = color;
+    canvas.moveTo(xInitial, yInitial);
+    canvas.lineTo(xFinal, yFinal);
+    canvas.stroke();
+    canvas.closePath();
+}
+
+for(counter; counter < lines; counter++) {
+    yi = 10 * counter;
+    xf = 10 * (counter + 1);
+    drawLine("black", 0, yi, xf, 300);
+}
+
+drawLine("red", 1, 1, 1, 299);
+drawLine("red", 1, 299, 299, 299);
