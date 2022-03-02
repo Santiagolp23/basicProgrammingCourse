4f878a87f0ee97c2fcbe99925bff6f27d40a7fcc
Santiagolp23 <santiagolopezpardo@gmail.com>
2022-03-01 21:52:39 -0500

    Drawing a curve using only lines on 2 opposing corners using clever trick LOL

--- canvas/drawing.js
+++ canvas/drawing.js
@@ -18,7 +18,10 @@ for(counter; counter < lines; counter++) {
     yi = 10 * counter;
     xf = 10 * (counter + 1);
     drawLine("black", 0, yi, xf, 300);
+    drawLine("black", yi, 0, 300, xf); //Clever trick to do the same at the opposite corner LOL
 }
 
 drawLine("red", 1, 1, 1, 299);
 drawLine("red", 1, 299, 299, 299);
+drawLine("red", 299, 1, 1, 1);
+drawLine("red", 299, 299, 299, 1);
