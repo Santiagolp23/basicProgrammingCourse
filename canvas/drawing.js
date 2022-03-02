0b5a7e11aa853c838820cecc82e0e93813f403d6
Santiagolp23 <santiagolopezpardo@gmail.com>
2022-03-02 18:31:38 -0500

    Drawing a curve using only lines, amount  of lines is variable by user input.

--- canvas/drawing.js
+++ canvas/drawing.js
@@ -1,8 +1,11 @@
 var d = document.getElementById("drawing");
 var canvas = d.getContext("2d");
-var lines = 30;
-var counter = 0;
-var yi, xf;
+var width = d.width;
+
+var numberLinesUser = document.getElementById("userInputLines");
+var button = document.getElementById("userButton");
+button.addEventListener("click", drawingByCLick);
+
 
 function drawLine(color, xInitial, yInitial, xFinal, yFinal) {
 
@@ -14,14 +17,22 @@ function drawLine(color, xInitial, yInitial, xFinal, yFinal) {
     canvas.closePath();
 }
 
-for(counter; counter < lines; counter++) {
-    yi = 10 * counter;
-    xf = 10 * (counter + 1);
-    drawLine("black", 0, yi, xf, 300);
-    drawLine("black", yi, 0, 300, xf); //Clever trick to do the same at the opposite corner LOL
-}
+function drawingByCLick() {
+    var lines = parseInt(numberLinesUser.value);
+    var counter = 0;
+    var space = width / lines;
 
-drawLine("red", 1, 1, 1, 299);
-drawLine("red", 1, 299, 299, 299);
-drawLine("red", 299, 1, 1, 1);
-drawLine("red", 299, 299, 299, 1);
+
+    for(counter; counter < lines; counter++) {
+        yi = space * counter;
+        xf = space * (counter + 1);
+        drawLine("black", 0, yi, xf, 300);
+        drawLine("black", yi, 0, 300, xf); //Clever trick to do the same at the opposite corner LOL
+    }
+
+    drawLine("red", 1, 1, 1, (width - 1));
+    drawLine("red", 1, (width - 1), (width - 1), (width - 1));
+    drawLine("red", 299, (width - 1), (width - 1), (width - 1));
+    drawLine("red", (width - 1), (width - 1), (width - 1), 1);
+
+}
