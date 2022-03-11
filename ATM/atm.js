91ae2cde6bb40a55db8c14f0b6a4d361544a9265
Santiagolp23 <santiagolopezpardo@gmail.com>
2022-03-10 22:21:33 -0500

    Building an ATM using javaScript

---
+++ ATM/atm.js
@@ -0,0 +1,17 @@
+class Bill {
+    constructor(v, q){
+        this.value = v;
+        this.quantity = v;
+    }
+}
+
+var register = [];
+var delivered = [];
+register.push(new Bill (50, 3));
+register.push(new Bill (20, 2));
+register.push(new Bill (10, 2));
+
+var money = 210;
+var div = 0;
+
+
